bfaa4e7e921db80167e169eb707bcda71c4b9c31
Sakriya Khadka <Khadkasakriya81@gamil.com>
2024-08-23 17:12:11 +0545

    deployment

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000","https://domainname.com"], // The frontend address
+    origin:"https://domainname.com", // The frontend address
     methods: ["GET", "POST"], // Allowable methods
     credentials: true, 
   },
