0a1708dc207b5988c04eec49c191c4658f491532
Sakriya Khadka <Khadkasakriya81@gamil.com>
2024-08-23 21:22:09 +0545

    ysse

--- src/server.ts
+++ src/server.ts
@@ -32,12 +32,14 @@ const app = Express();
 
 const httpServer = createServer(app);
 
-const io = new Server(httpServer, {
-  cors:{
-    origin:"*",
-    methods:["GET","POST"]
-  }
-});
+const io = new Server(httpServer, 
+//   {
+//   cors:{
+//     origin:"*",
+//     methods:["GET","POST"]
+//   }
+// }
+);
 
 // io.use(socketCorsMiddleware);
 
