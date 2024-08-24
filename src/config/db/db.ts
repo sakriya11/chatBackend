97fbbdbb1e21234daf8293741ae069987911db40
Sakriya Khadka <Khadkasakriya81@gamil.com>
2024-08-24 20:19:48 +0545

    changes in db configuration

--- src/config/db/db.ts
+++ src/config/db/db.ts
@@ -3,10 +3,7 @@ import dbConfig from '../index';
 
 const MONGO_URL = dbConfig.db.mongoURL;
 
-mongoose.connect(MONGO_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}as ConnectOptions).catch(console.error);
+mongoose.connect(MONGO_URL).catch(console.error);
 
 const db = mongoose.connection;
 
